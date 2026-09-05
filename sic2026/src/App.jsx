fcbc055b18f8392ff3fb1bdda828b9fe9019398f
HARSHEND12 <hgiri1957@gmail.com>
2026-09-05 12:30:35 +0530

    Mistake Route path for checkin page.

--- sic2026/src/App.jsx
+++ sic2026/src/App.jsx
@@ -7,6 +7,7 @@ import Admin from './pages/Admin.jsx'
 import Nav from './components/Nav.jsx'
 import Footer from './components/Footer.jsx'
 import CursorDot from './components/CursorDot.jsx'
+import Checkin from './pages/Checkin.jsx'
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <main className="flex-1">
         <CursorDot />
         <Routes>
+          <Route path="/checkin" element={<Checkin />} />
           <Route path="/" element={<Landing />} />
           <Route path="/register" element={<Register />} />
           <Route path="/ticket/:ticketId" element={<Ticket />} />
